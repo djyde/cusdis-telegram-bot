590b40eb6b7697fc2a9942722479ba65f628475d
Randy <randypriv@gmail.com>
2023-02-12 14:43:52 +0800

    add railway host

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 const Bot = require("node-telegram-bot-api");
-const token = process.env.TOKEN
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const token = process.env.TOKEN;
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const bot = new Bot(token, {
   polling: true,
@@ -14,11 +14,15 @@ const bot = new Bot(token, {
 
 const env = {
   token: process.env.TOKEN,
-  host: process.env.HOST || 'https://tg-bot.cusdis.com'
+  host: process.env.HOST || "https://tg-bot.cusdis.com",
+};
+
+if (process.env.RAILWAY_STATIC_URL) {
+  env.host = `https://${process.env.RAILWAY_STATIC_URL}`;
 }
 
 bot.onText(/\/gethook/, (msg) => {
-  console.log(msg)
+  console.log(msg);
   bot.sendMessage(
     msg.chat.id,
     `Click to copy your Telegram Webhook URL:
@@ -29,46 +33,46 @@ bot.onText(/\/gethook/, (msg) => {
   );
 });
 
-app.get('/', (req, res) => {
-  res.send('works')
-})
+app.get("/", (req, res) => {
+  res.send("works");
+});
 
-app.post('/api/hook/:chatId', async (req) => {
-  const { chatId } = req.params
-    const { type, data } = req.body
+app.post("/api/hook/:chatId", async (req) => {
+  const { chatId } = req.params;
+  const { type, data } = req.body;
 
-    switch (type) {
-      case "new_comment": {
-        const msg = `New comment on website <strong>${
-          data.project_title
-        }</strong> in page <strong>${data.page_title}</strong>:
+  switch (type) {
+    case "new_comment": {
+      const msg = `New comment on website <strong>${
+        data.project_title
+      }</strong> in page <strong>${data.page_title}</strong>:
 <pre>
 ${data.content.replace(/<[^>]*>?/gm, "")}
 </pre>
 by: <strong>${data.by_nickname}</strong>`;
-        await sendMessage(chatId, msg, {
-          reply_markup: {
-            inline_keyboard: [
-              [
-                {
-                  text: "Approve without login",
-                  url: data.approve_link,
-                },
-              ],
+      await sendMessage(chatId, msg, {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Approve without login",
+                url: data.approve_link,
+              },
             ],
-          },
-        });
-        break;
-      }
+          ],
+        },
+      });
+      break;
     }
+  }
 
-    res.json({
-      msg: "works",
-    });
-})
+  res.json({
+    msg: "works",
+  });
+});
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log('running on port:', port)
-})
+  console.log("running on port:", port);
+});
