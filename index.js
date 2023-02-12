382d8f907475f481c36bd2bb7e41df5c56e7aef2
Randy <randypriv@gmail.com>
2023-02-12 14:01:27 +0800

    use express

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const Bot = require("node-telegram-bot-api");
 const token = process.env.TOKEN
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 const bot = new Bot(token, {
   polling: true,
 });
 
 bot.onText(/\/gethook/, (msg) => {
+  console.log(msg)
   bot.sendMessage(
     msg.chat.id,
     `Click to copy your Telegram Webhook URL:
@@ -15,3 +23,43 @@ bot.onText(/\/gethook/, (msg) => {
     }
   );
 });
+
+app.post('/api/hook/:chatId', async (req) => {
+  const { chatId } = req.params
+    const { type, data } = req.body
+
+    switch (type) {
+      case "new_comment": {
+        const msg = `New comment on website <strong>${
+          data.project_title
+        }</strong> in page <strong>${data.page_title}</strong>:
+<pre>
+${data.content.replace(/<[^>]*>?/gm, "")}
+</pre>
+by: <strong>${data.by_nickname}</strong>`;
+        await sendMessage(chatId, msg, {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Approve without login",
+                  url: data.approve_link,
+                },
+              ],
+            ],
+          },
+        });
+        break;
+      }
+    }
+
+    res.json({
+      msg: "works",
+    });
+})
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+  console.log('running on port:', port)
+})
